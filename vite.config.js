435992e40c136fa131914d5969b376ae791d2fb0
Venomity <33787357+veetihietasalo@users.noreply.github.com>
2023-12-04 00:15:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personalTrainerAPIsite/', // Add this row and use your own repository name
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  plugins: [react()]
 })
