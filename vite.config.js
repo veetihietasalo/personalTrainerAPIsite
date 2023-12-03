0e00d54811fc712448be24f5eb8fe308ae6c8299
Venomity <veeti.hietasalo@hotmail.fi>
2023-12-03 23:19:22 +0200

    index.html lisäys gh-pagesia varten

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personalTrainerAPIsite/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+},
 })
